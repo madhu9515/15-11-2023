92217f77c9bb0cb003e1f7f6c0ad95d3ab7ba6bc
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-15 22:17:39 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,20 @@
+n=5
+for a in range(n):
+    for b in range(n):
+        print('*',end=' ')
+    print(' ')
+
+print(' ')
+for x in range(1,n+1):
+    print('*'*x)
+print(' ')
+for d in range(n):
+    for e in range(n+1):
+        print('*',end=' ')
+    for f in range() 
+    print(' ')
+
+
+
+    
+    
